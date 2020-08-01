c8d394a283cc9781405a88cc400e080e02d3c035
Peter Corcoran <peter.corcoran@shadowacre.ltd>
2020-08-01 12:46:46 +0100

    Added ability to start nuxt with `yarn dev-nuxt`/`npm run dev-nuxt`, this required some changes to provide more sane defaults in the environment inject script.

--- src-nuxt/plugins/envInject.js
+++ src-nuxt/plugins/envInject.js
@@ -1,15 +1,29 @@
-export default (context, inject) => {
-  const api    = process.env.api || {}
-  const config = process.env.config || {}
+const addSaneDefaults = api => {
+  api.info = {
+    title: "Untitled",
+    version: "0.0.0",
+    description: "",
+    ...api.info
+  }
+}
 
+const addPathMethodAndDefaultTags = api => {
   for(let path in api.paths) {
     for(let method in api.paths[path]) {
       const def = api.paths[path][method]
       def.path = path
       def.method = method
+
+      if(typeof def.tags !== 'array' || def.tags.length == 0) {
+        def.tags = ['default']
+        if(api.tags.filter(t => t.name == 'default').length == 0)
+          api.tags.push({ name: "default" })
+      }
     }
   }
+}
 
+const addPathsToTags = api => {
   api.tags.forEach(tag => {
     tag.paths = {}
     for(let path in api.paths) {
@@ -23,10 +37,32 @@ export default (context, inject) => {
       }
     }
   })
+}
 
-  console.log(`API name: ${api.info.title}, Version: ${api.info.version}`)
+export default (context, inject) => {
+  const config = {
+    i18n: {},
+    ...(process.env.config || {})
+  }
+
+  const api = {
+    info: {},
+    servers: [],
+    security: [],
+    paths: {},
+    tags: [],
+    externalDocs: [],
+    components: {},
+    ...(process.env.api || {})
+  }
+
+  addSaneDefaults(api)
+  addPathMethodAndDefaultTags(api)
+  addPathsToTags(api)
 
   inject('api',    api)
   inject('config', config)
   inject('i18n',   key => config.i18n[key] || key)
+
+  console.log(`API name: ${api.info.title}, Version: ${api.info.version}`)
 }
